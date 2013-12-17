369cbe6edf7e489cbba9297271cbe26114293875
PeiYao wang <yao@radxa.com>
2013-12-17 07:34:50 -0800

    delete one line add one line

--- Launcher2/Android.mk
+++ Launcher2/Android.mk
@@ -12,13 +12,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
 # 3   
 # 2
 # 4
 # 5
 # 6
 LOCAL_PATH := $(call my-dir)
+#7
 
 include $(CLEAR_VARS)
 
